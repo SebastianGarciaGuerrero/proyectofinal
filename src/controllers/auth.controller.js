10af67550a0a65d7d343e599983b930e7bfb17da
Sebastian Garcia Guerrero <seba.garcia.g@gmail.com>
2024-01-02 16:38:39 -0300

    controllers añadidos

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,5 @@
+
+export const signin = (req, res) => res.send('ingresando')
+export const signup = (req, res) => res.send('registrando')
+export const signout = (req, res) => res.send('cerrando sesión')
+export const profile = (req, res) => res.send('perfil de usuario')
