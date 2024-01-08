0929543d994f774e13e757c36acb6f8f67ecf7ce
Sebastian Garcia Guerrero <seba.garcia.g@gmail.com>
2024-01-08 18:21:18 -0300

    jwt y signups

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,35 @@
+import bcrypt from "bcrypt";
+import { pool } from "../db.js";
+import { createAccessToken } from "../libs/jwt.js";
+
 export const signin = (req, res) => res.send("ingresando");
-export const signup = (req, res) => res.send("registrando");
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const result = await pool.query(
+      "INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *",
+      [name, email, hashedPassword]
+    );
+
+    const token = await createAccessToken({ id: result.rows[0].id });
+
+    //return res.json(result.rows[0]);
+    return res.json({
+      token: token,
+    });
+  } catch (error) {
+    if (error.code === "23505") {
+      return res.status(400).json({
+        message: " El email ya está registrado o_O",
+      });
+    }
+  }
+};
+
 export const signout = (req, res) => res.send("cerrando sesión");
+
 export const profile = (req, res) => res.send("perfil de usuario");
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,19 @@
-import { Router } from 'express'
-import {signin, signout, signup, profile} from '../controllers/auth.controller.js'
+import Router from "express-promise-router";
+import {
+  signin,
+  signout,
+  signup,
+  profile,
+} from "../controllers/auth.controller.js";
 
-const router = Router()
+const router = Router();
 
-router.post('/signin', signin)
+router.post("/signin", signin);
 
-router.post('/signup', signup)
+router.post("/signup", signup);
 
-router.post('/signout', signout)
+router.post("/signout", signout);
 
-router.get('/profile', profile)
+router.get("/profile", profile);
 
-export default router
+export default router;
