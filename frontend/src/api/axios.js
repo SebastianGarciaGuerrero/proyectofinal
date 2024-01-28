dba2a2a0477802422e2ce3104154a509bd749749
Sebastian Garcia Guerrero <seba.garcia.g@gmail.com>
2024-01-28 18:15:07 -0300

    tasks aun falta :(

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL:'http://localhost:3000/api',
-    withCredentials: true,
-})
+  baseURL: "http://localhost:3000/api",
+  withCredentials: true,
+});
 
-export default client
+export default client;
--- frontend/src/pages/TaskFormPage.jsx
+++ frontend/src/pages/TaskFormPage.jsx
@@ -1,6 +1,40 @@
+import { Card, Input, Label, Textarea, Button } from "../components/ui";
+import { useForm } from "react-hook-form";
+import { createTaskRequest } from "../api/tasks.api";
 
 export const TaskFormPage = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit(async (data) => {
+    console.log(data);
+    const res = await createTaskRequest(data);
+    console.log(res);
+  });
+
   return (
-    <div>TaskFormPage</div>
-  )
-}
+    <div className="flex h-[80vh] justify-center items-center">
+      <Card>
+        <h2 className="text-3xl font-bold my-4">Create Task</h2>
+        <form onSubmit={onSubmit}>
+          <Label htmlFor="title">Title</Label>
+          <Input
+            type="text"
+            placeholder="Title"
+            autoFocus
+            {...register("title", {
+              required: true,
+            })}
+          />
+
+          <Label htmlFor="description">Description</Label>
+          <Textarea
+            placeholder="Description"
+            rows={3}
+            {...register("description")}
+          ></Textarea>
+          <Button>Create</Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
