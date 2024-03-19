42e03cbaf8e168c0ae36cf28c51845184f594728
Sebastian Garcia Guerrero <seba.garcia.g@gmail.com>
2024-03-19 11:34:22 -0300

    arreglando errors

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const PORT = process.env.PORT || 3000;
 
-export const PG_PORT = process.env.PORT || 5432;
-export const PG_HOST = process.env.HOST || "localhost";
-export const PG_USER = process.env.USER || "postgres";
-export const PG_PASSWORD = process.env.PASSWORD || "1503segg";
-export const PG_DATABASE = process.env.DATABASE || "tasksdb";
+export const PG_PORT = process.env.PG_PORT || 5432;
+export const PG_HOST = process.env.PG_HOST || "localhost";
+export const PG_USER = process.env.PG_USER || "postgres";
+export const PG_PASSWORD = process.env.PG_PASSWORD || "1503segg";
+export const PG_DATABASE = process.env.PG_DATABASE || "tasksdb";
 
 export const ORIGIN = process.env.ORIGIN || "http://localhost:5173";
