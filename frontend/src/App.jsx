0cc2573887be59780366a5d184ac8bb63b003d68
Sebastian Garcia Guerrero <seba.garcia.g@gmail.com>
2024-05-31 16:25:59 -0400

    arreglado problema de perfil y nuevo producto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route, Outlet } from "react-router-dom";
 import { AuthProvider, AuthContext } from "./context/AuthProvider";
 import { ProductProvider } from "./context/ProductProvider";
 
-
 import { Navbar } from "./components/navbar/Navbar";
 import { Footer } from "./components/footer/Footer";
 import { Container } from "./components/ui";
@@ -26,30 +25,25 @@ function App() {
 
   return (
     <>
-     
       <ProductProvider>
         <AuthProvider>
-        <Navbar />
-        <Container className="py-5">
-          <Routes>
-            <Route
-              element={
-                <ProtectedRoute isAllowed={!isAuth} redirectTo={"/products"} />
-              }
-            >
-              <Route path="/" element={<HomePage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/products" element={<ProductsPage />} />
-            </Route>
-
-            <Route
-              element={
-                <ProtectedRoute isAllowed={isAuth} redirectTo={"/login"} />
-              }
-            >
-              <Route element={<Outlet />}>
+          <Navbar />
+          <Container className="py-5">
+            <Routes>
+              <Route
+                element={
+                  <ProtectedRoute
+                    isAllowed={!isAuth}
+                    redirectTo={"/products"}
+                  />
+                }
+              >
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/products" element={<ProductsPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
                 <Route path="/products/new" element={<ProductFormPage />} />
                 <Route
                   path="/products/:id/edit"
@@ -57,12 +51,9 @@ function App() {
                 />
               </Route>
 
-              <Route path="/profile" element={<ProfilePage />} />
-            </Route>
-
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Container>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Container>
         </AuthProvider>
       </ProductProvider>
 
